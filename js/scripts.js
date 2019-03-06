548182b4dea94eeacce51575751925b2badaf9b4
Mitch <mkelbourne@gmail.com>
2019-03-06 14:14:27 +0000

    Content audits across the page, awaiting asset changes

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,6 @@ function scrollToConfig() {
 
 	var subNavLinks = document.querySelectorAll(".nav-link");
 
-
 	subNavLinks.forEach(function(item){
 
 		var hash = item.href.substring(item.href.indexOf("#")+1);
@@ -21,6 +20,28 @@ function scrollToConfig() {
 			e.preventDefault();
 		}
 
+
 	})
 }
 scrollToConfig();
+
+function inlineLinkSroll() {
+	var inlineLink = document.querySelectorAll(".inline-tag");
+
+	inlineLink.forEach(function(item){
+
+		var hash = item.href.substring(item.href.indexOf("#")+1);
+
+		item.onclick = function(e) {
+
+			$(window).scrollTo(
+				item.hash, {
+				duration: 500,
+				offset: (300)
+			}) 
+			e.preventDefault();
+		}
+	})
+
+}
+inlineLinkSroll();
